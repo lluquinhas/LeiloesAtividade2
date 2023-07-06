9dec7c0ef9fe6a25af31f47f98431bafa34d1cc8
Lucas Miqueloti <xbrlucasx@Gmail.com>
2023-07-06 15:55:46 -0300

    refazendo o código

--- src/Classes/ProdutosDAO.java
+++ src/Classes/ProdutosDAO.java
@@ -13,43 +13,26 @@ import java.util.List;
 public class ProdutosDAO {
 
    Connection conn;
-    PreparedStatement st;
-    ResultSet rs;
-    // private static final List<ProdutosDTO> lista = new ArrayList<>();
-
-   public List<ProdutosDTO> listagem(String filtro) {
-
-        String sql = "select * from produtos";
-
-        if (!filtro.isEmpty()) {
-            sql = sql + " where nome like ?";
-        }
-
-        try {
-            st = conn.prepareStatement(sql);
-
-            if (!filtro.isEmpty()) {
-                st.setString(1,"%" + filtro + "%");
-            }
-
-            rs = st.executeQuery();
-            List<ProdutosDTO> lista = new ArrayList<>();
-
-            while (rs.next()) {
-                ProdutosDTO dto = new ProdutosDTO();
-                dto.setId(rs.getString("id"));
-                dto.setNome(rs.getString("nome"));
-                dto.setValor(rs.getString("valor"));
-                dto.setStatus(rs.getString("status"));
-                lista.add(dto);
-            }
-            return lista;
-        } catch (SQLException ex) {
-            System.out.println("Erro ao conectar: " + ex.getMessage());
-            return null;
-        }
+    PreparedStatement prep;
+    ResultSet resultset;
+    ArrayList<ProdutosDTO> listagem = new ArrayList<>();
+    
+    public void cadastrarProduto (ProdutosDTO produto){
+        
+        
+        //conn = new conectaDAO().connectDB();
+        
+        
+    }
+    
+    public ArrayList<ProdutosDTO> listarProdutos(){
+        
+        return listagem;
+    }
+    
+    
+    
+        
    }
    
-   
 
-}
